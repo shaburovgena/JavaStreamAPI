18849ce931d6365480e641d5b56b1d08058d9f61
shaburovgena <shaburovgena@gmail.com>
2019-10-24 11:50:36 +0300

    Add comment

--- src/main/java/com/example/Event.java
+++ src/main/java/com/example/Event.java
@@ -1,41 +1,19 @@
 package com.example;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
-
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@ToString(of = {"id", "timeTag", "description"})
 public class Event {
     private UUID id;
     private LocalDateTime timeTag;
     private String description;
 
-    public Event(UUID id, LocalDateTime timeTag, String description) {
-        this.id = id;
-        this.timeTag = timeTag;
-        this.description = description;
 
-        System.out.printf("Generated %s", id.toString());
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getTimeTag() {
-        return timeTag;
-    }
-
-    public void setTimeTag(LocalDateTime timeTag) {
-        this.timeTag = timeTag;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- src/test/java/com/example/APIExamples.java
+++ src/test/java/com/example/APIExamples.java
@@ -2,7 +2,6 @@ package com.example;
 
 import org.junit.Test;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 import java.nio.file.Files;
@@ -46,9 +45,9 @@ public class APIExamples {
     @Test
     public void creationExamples() throws IOException {
 
-        Stream<String> lines = Files.lines(Paths.get("someStream.txt"));
-        Stream<Path> list = Files.list(Paths.get("./"));
-        Stream<Path> walk = Files.walk(Paths.get("./"), 3);
+        Stream<String> lines = Files.lines(Paths.get("someStream.txt"));//Стрим на основе файла
+        Stream<Path> list = Files.list(Paths.get("./"));//Обходим все дерево директорий
+        Stream<Path> walk = Files.walk(Paths.get("./"), 3);//Обходим дерево директорий, ограниченное 3 вложенными папками
 
         IntStream intStream = IntStream.of(1, 2, 3, 4);
         DoubleStream doubleStream = DoubleStream.of(1, 2, 3, 4);
@@ -56,36 +55,37 @@ public class APIExamples {
         IntStream intStream1 = IntStream.rangeClosed(10, 100); //10 ... 100
 
         int[] ints = {1, 2, 3, 4};
-        IntStream stream = Arrays.stream(ints);
+        IntStream stream = Arrays.stream(ints);//Стрим на основе массива
 
-        Stream<String> stringStream = Stream.of("1", "2", "3");
-        Stream<? extends Serializable> streamSerializable = Stream.of(1, 2.1, "3");
+        Stream<String> stringStream = Stream.of("1", "2", "3");//Стрим на основе строк(и)
+        Stream<? extends Serializable> streamSerializable = Stream.of(1, 2.1, "3");//Стрим из сериализуемых элементов
 
         Stream<String> build = Stream.<String>builder()
                 .add("one")
                 .add("two")
                 .add("three")
-                .build();
+                .build();//Создание стрима с помощью билдера
     }
 
     @Test
-    public void creationFromCollectionsExamples() throws IOException {
+    public void creationFromCollectionsExamples()  {
         Stream<Employee> stream = emps.stream();
         Stream<Employee> employeeStream = emps.parallelStream();
 
-        Stream.concat(stream, employeeStream);
+        Stream.concat(stream, employeeStream);//Объединение стримов
     }
 
     @Test
-    public void creationAsGenerationExamples() throws IOException {
-        Stream<Event> generate = Stream.generate(() ->
-                new Event(UUID.randomUUID(), LocalDateTime.now(), ""));
+    public void creationAsGenerationExamples()  {
+        Stream<Event> generate = Stream.generate(() ->//Создание стрима с помощью генерации
+                new Event(UUID.randomUUID(), LocalDateTime.now(), "")).limit(1000);
+        generate.forEach(System.out::println);
     }
 
     @Test
-    public void creationAsIterationExamples() throws IOException {
-        Stream<Integer> iterate = Stream.iterate(1950, val -> val + 3);
-
+    public void creationAsIterationExamples()  {
+        Stream<Integer> iterate = Stream.iterate(1950, val -> val + 3).limit(1000);//Создание стрима с помощью итерации
+        iterate.forEach(System.out::println);
     }
 
     @Test
@@ -97,83 +97,106 @@ public class APIExamples {
         emps.forEach(employee -> System.out.println(employee.getId()));
         emps.stream().forEachOrdered(employee -> System.out.println(employee.getAge()));
 
-        List<Employee> collect = emps.stream().collect(Collectors.toList());
-        Object[] objects = emps.stream().toArray();
-        Map<Integer, String> empsMap = emps.stream().collect(Collectors.toMap(
-                employee -> employee.getId(),
-                employee -> String.format("%s %s", employee.getFirstName(), employee.getLastName())
+        List<Employee> collect = emps.stream().collect(Collectors.toList());//Создание коллекции Лист из стрима
+        Object[] objects = emps.stream().toArray();//Создание массива из стрима
+        Map<Integer, String> empsMap = emps.stream().collect(Collectors.toMap(//Создание Мапы из стрима
+                employee -> employee.getId(),//Ключом коллекции будет id
+                employee -> String.format("%s %s", employee.getFirstName(), employee.getLastName())//Значение - имя и фамилия
         ));
 
         System.out.println(collect.get(0).getFirstName());
         System.out.println(empsMap.get(243));
 
         IntStream intStream = IntStream.of(100, 200, 300, 400);
-        OptionalInt reduceResult = intStream.reduce(((left, right) -> left + right));
-        System.out.println(reduceResult.getAsInt());
+        OptionalInt reduceResult = intStream.reduce(((left, right) -> left + right));//Сложение элементов стрима
+        System.out.println(reduceResult.getAsInt());//1000
 
     }
 
     @Test
     public void mathExamples() {
-        System.out.println(IntStream.of(100, 200, 300, 400).average());
-        System.out.println(IntStream.of(100, 200, 300, 400).max());
-        System.out.println(IntStream.of(100, 200, 300, 400).min());
-        System.out.println(IntStream.of(100, 200, 300, 400).sum());
-        System.out.println(IntStream.of(100, 200, 300, 400).summaryStatistics());
-        Optional<Employee> maxAgeWithComparator = emps.stream().max(Comparator.comparingInt(Employee::getAge));
+        System.out.println(IntStream.of(100, 200, 300, 400).average());//Среднее значение всех элементов
+        System.out.println(IntStream.of(100, 200, 300, 400).max());//Максимальное значение
+        System.out.println(IntStream.of(100, 200, 300, 400).min());//Минимальное значение
+        System.out.println(IntStream.of(100, 200, 300, 400).sum());//Сумма всех элементов
+        System.out.println(IntStream.of(100, 200, 300, 400).summaryStatistics());//Статистика (перечисленное выше)
+        Optional<Employee> maxAgeWithComparator = emps
+                .stream()
+                .max(Comparator.comparingInt(Employee::getAge));//Максимальное значение элемента через Компаратор
         System.out.println(maxAgeWithComparator.get().getFirstName());
 
-        Optional<Employee> maxAgeWithLambda = emps.stream().max((emloyee1, employee2) -> emloyee1.getAge() - employee2.getAge());
+        Optional<Employee> maxAgeWithLambda = emps
+                .stream()
+                .max((emloyee1, employee2) ->
+                        emloyee1.getAge() - employee2.getAge());//Максимальное значение элемента через Лямбду
         System.out.println(maxAgeWithLambda.get().getFirstName());
 
-        Optional<Employee> randomEmployee = emps.stream().findAny();
+        Optional<Employee> randomEmployee = emps.stream().findAny();//Поиск любого элемента стрима
         System.out.println(randomEmployee.get().getFirstName());
-        Optional<Employee> firstEmployee = emps.stream().findFirst();
+        Optional<Employee> firstEmployee = emps.stream().findFirst();//Поиск первого элемента стрима
         System.out.println(firstEmployee.get().getFirstName());
 
 
-        boolean isLessSixteYO = emps.stream().noneMatch((employee -> employee.getAge() > 60)); //true
-        boolean isOlderEighteenYO = emps.stream().allMatch((employee -> employee.getAge() > 18)); //true
-        boolean isEmplShef = emps.stream().anyMatch((employee -> employee.getPosition() == Position.CHEF)); //true
+        boolean isLessSixteYO = emps
+                .stream()
+                .noneMatch((employee ->
+                employee.getAge() > 60)); //true если нет совпадение с условием
+        boolean isOlderEighteenYO = emps
+                .stream()
+                .allMatch((employee ->
+                        employee.getAge() > 18)); //true если все элементы попадают под условие
+        boolean isEmplShef = emps
+                .stream()
+                .anyMatch((employee ->
+                        employee.getPosition() == Position.CHEF)); //true один элемент попадает под условие
 
     }
 
     @Test
     public void transform() {
-        LongStream longStream = IntStream.of(100, 200, 300, 400).mapToLong(Long::valueOf);
+        LongStream longStream = IntStream.of(100, 200, 300, 400)
+                .mapToLong(Long::valueOf);//Преобразование стрима с Int в Long
 
-        Stream<Event> eventStream = IntStream.of(100, 200, 300, 400).mapToObj(value ->
-                new Event(UUID.randomUUID(), LocalDateTime.of(value, 12, 1, 12, 0), ""));
+        Stream<Event> eventStream = IntStream.of(100, 200, 300, 400)
+                .mapToObj(value ->
+                new Event(
+                        UUID.randomUUID(),
+                        LocalDateTime.of(value, 12, 1, 12, 0),
+                        ""));//Преобразование стрима с Int в Object с параметрами value
         eventStream.forEachOrdered(event -> System.out.println(event.getTimeTag()));
 
-        Stream<Employee> skipThreeEmployee = emps.stream().skip(3);
-        Stream<Employee> limitFirstFiveEmployee = emps.stream().limit(5);
+        Stream<Employee> skipThreeEmployee = emps.stream().skip(3);//Пропуск первых 3 элементов
+        Stream<Employee> limitFirstFiveEmployee = emps.stream().limit(5);//Ограничение 5ю элементами
 
         emps.stream()
                 .skip(3)
                 .limit(5).forEachOrdered(employee -> System.out.println(employee.getFirstName()));
 
-        System.out.println();
+        System.out.println("sorted");
         emps.stream()
                 .sorted(Comparator.comparingInt(Employee::getAge))/*(employee1, employee2) -> employee1.getAge()-employee2.getAge()*/
-                .forEach(System.out::println);
+                .forEach(System.out::println);//Сортировка элементов по значениям (в данном случае по возрасту)
 
 
-        System.out.println();
+        System.out.println("takeWhile");
         emps.stream()
-                .takeWhile(employee -> employee.getAge() > 30).forEach(System.out::println);//Стрим отрабатывает пока не совпадет условие
+                .takeWhile(employee -> employee.getAge() > 30)
+                .forEach(System.out::println);//Стрим отрабатывает пока не совпадет условие
 
-        System.out.println();
+        System.out.println("dropWhile");
         emps.stream()
-                .dropWhile(employee -> employee.getAge() > 30).forEach(System.out::println);//Стрим не отрабатывает пока не совпадет условие
+                .dropWhile(employee -> employee.getAge() > 30)
+                .forEach(System.out::println);//Стрим не отрабатывает пока не совпадет условие
 
-        System.out.println();
+        System.out.println("peek");
         emps.stream()
-                .peek(employee -> employee.setAge(18)).forEach(System.out::println);//Заменяем всем элементам определенное значение
+                .peek(employee -> employee.setAge(18))
+                .forEach(System.out::println);//Заменяем всем элементам определенное значение
 
-        System.out.println();
+        System.out.println("flatMap");
         IntStream.of(100, 200, 300, 400)
-                .flatMap(value -> IntStream.of(value - 50, value)).forEach(System.out::println);//Добавляем промежуточные значения
+                .flatMap(value -> IntStream.of(value - 50, value))
+                .forEach(System.out::println);//Добавляем промежуточные значения
     }
 
     @Test
@@ -215,7 +238,7 @@ public class APIExamples {
 
     @Test
     public void reduceExample() {
-        System.out.println(deps.stream().reduce(this::reducer));
+        System.out.println(deps.stream().reduce(this::reducer));//Создает дерево объектов на основе подчиненности
     }
 
     public Department reducer(Department parent, Department child) {
